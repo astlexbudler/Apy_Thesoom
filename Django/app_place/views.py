0b7c7fba2fac42ff92ff4195143849c8c5e28304
hansy <hansy9151@naver.com>
2025-02-19 01:17:13 +0900

    place front, backend

--- Django/app_place/views.py
+++ Django/app_place/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from app_core import daos as do
+from app_core import models as mo
 
 # 장소 정보 상세 페이지
 def place_detail(request):
@@ -10,4 +12,27 @@ def place_write(request):
 
 # 장소 수정 페이지
 def place_edit(request):
-    return render(request, 'place/edit.html')
+    place_id = request.GET.get("place_id")
+
+    if not place_id:
+        # place_id가 없으면 새로운 장소 생성
+        new_place = do.create_place(
+            name="",
+            intro="",
+            location="",
+            location_link="",
+            description="",
+            status="writing"
+        )
+
+        if new_place["success"]:
+            place_id = new_place["place_id"]
+        else:
+            return render(request, 'place/edit.html', {"error": new_place["message"]})
+
+    try:
+        place = mo.PLACE.objects.get(id=place_id)
+    except mo.PLACE.DoesNotExist:
+        return render(request, 'place/edit.html', {"error": "해당 장소를 찾을 수 없습니다."})
+
+    return render(request, 'place/edit.html', {"place": place})
